229407d43df50bee336a433c92a805bc0ec27dc4
Gegory357 <101174423+Gregory313@users.noreply.github.com>
2024-12-12 14:39:38 +0700

    Create backupDb.sh

---
+++ backupDb.sh
@@ -0,0 +1,15 @@
+USER="root"
+HOST="localhost"
+PORT=5432
+PASSWORD="pass" 
+
+export PGPASSWORD=$PASSWORD
+
+databases=$(psql -U $USER -h $HOST -p $PORT -d postgres -t -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
+
+for db in $databases; do
+    echo "Making dump for : $db"
+    pg_dump -U $USER -h $HOST -p $PORT -f "/tmp/${db}_dump.sql" $db
+done
+
+unset PGPASSWORD
